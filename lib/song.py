86da146d52dc7afcbf397673fcc1ed0a616e8b1e
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-06-09 13:25:00 +0300

    feat: implement add_to_artists with uniqueness check

--- lib/song.py
+++ lib/song.py
@@ -17,3 +17,8 @@ class Song:
     def add_to_genres(cls, genre):
         if genre not in cls.genres:
             cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
