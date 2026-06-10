4ee100544881d87b7b5735bc93d49f39e9c66259
iankinoti-cloud <ian.kinoti@student.moringaschool.com>
2026-06-10 11:15:00 +0300

    feat: implement add_to_genre_count class method

--- lib/song.py
+++ lib/song.py
@@ -26,3 +26,10 @@ class Song:
     def add_to_artists(cls, artist):
         if artist not in cls.artists:
             cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
